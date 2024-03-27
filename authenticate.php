93a1cf74a0538ceaf371b9d41bdaedf360a53812
Lashman Singh <lashmansingh38034@gmail.com>
2024-03-27 02:55:04 -0500

    Posting authentication file on GitHub which includes login information of user

---
+++ authenticate.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+define('ADMIN_LOGIN', 'wally');
+define('ADMIN_PASSWORD', 'mypass');
+
+if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])
+    || ($_SERVER['PHP_AUTH_USER'] != ADMIN_LOGIN)
+    || ($_SERVER['PHP_AUTH_PW'] != ADMIN_PASSWORD)) {
+
+    header('HTTP/1.1 401 Unauthorized');
+    header('WWW-Authenticate: Basic realm="Task Management System"');
+    exit("Access Denied: Username and password required.");
+}
+?>
