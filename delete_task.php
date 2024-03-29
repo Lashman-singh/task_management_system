c1c3393d1fe723a64263eebff9631acf93c143bf
Lashman Singh <lashmansingh38034@gmail.com>
2024-03-28 21:25:51 -0500

    fixed deleting task functionaly which was not working before

--- delete_task.php
+++ delete_task.php
@@ -2,24 +2,28 @@
 require('authenticate.php');
 require('connect.php');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['task_id'])) {
     try {
-        $comment_id = filter_input(INPUT_POST, 'comment_id', FILTER_SANITIZE_NUMBER_INT);
-
-        $query = "DELETE FROM Comment WHERE comment_id = :comment_id";
+        // Delete task
+        $task_id = filter_input(INPUT_POST, 'task_id', FILTER_SANITIZE_NUMBER_INT);
+        $query = "DELETE FROM task WHERE task_id = :task_id";
         $statement = $db->prepare($query);
-        $statement->bindParam(':comment_id', $comment_id, PDO::PARAM_INT);
+        $statement->bindValue(':task_id', $task_id, PDO::PARAM_INT);
+
+        // Execute the deletion query
         $statement->execute();
 
-        header('Location: index.php');
-        exit();
+        // Check if any rows were affected
+        if ($statement->rowCount() > 0) {
+            header("Location: index.php");
+            exit;
+        } else {
+            echo "Error: Task not found or already deleted.";
+        }
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
-        header('Location: index.php');
-        exit();
     }
 } else {
-    header('Location: index.php');
-    exit();
+    echo "<p>No task selected.</p>";
 }
 ?>
