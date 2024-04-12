ea3b3b4cae25ec9b3ed055f3c12e109b554b63e9
Lashman Singh <lashmansingh38034@gmail.com>
2024-04-12 18:22:02 -0500

    Updated add comment code, added CAPTCHA for varyfying human and robats

--- add_comment.php
+++ add_comment.php
@@ -5,13 +5,14 @@ require('connect.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $task_id = $_POST['task_id'];
     $comment_content = $_POST['comment'];
-    $user_id = $_SESSION['user_id'];
+    $employee_id = $_SESSION['employee_id']; 
 
-    $query = "INSERT INTO comment (task_id, user_id, comment) VALUES (:task_id, :user_id, :comment_content)";
+    $query = "INSERT INTO Comment (task_id, employee_id, comment) VALUES (:task_id, :employee_id, :comment_content)";
     $statement = $db->prepare($query);
-    $statement->execute(array(':task_id' => $task_id, ':user_id' => $user_id, ':comment_content' => $comment_content));
+    $statement->execute(array(':task_id' => $task_id, ':employee_id' => $employee_id, ':comment_content' => $comment_content));
 
     header('Location: index.php');
     exit();
 }
 ?>
+
