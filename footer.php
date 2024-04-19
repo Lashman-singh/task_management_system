704da14d8c3a288365c6226c435337288693d32f
Lashman Singh <lashmansingh38034@gmail.com>
2024-04-19 13:41:33 -0500

    updated inline css in footer bar for keeping the same design for all pages

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer>
+<footer style="background: linear-gradient(to right, #4e54c8, #8f94fb); color: #ffffff; text-align: center; padding: 20px 0; width: 100%; bottom: 0;">
     <div>
         <p>&copy; <?= date("Y") ?> Task Management System</p>
     </div>
